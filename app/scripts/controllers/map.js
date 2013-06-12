89f488622374b2c910565995977c639e9074be50
Sam Frons <samfrons@gmail.com>
2013-06-12 15:45:23 -0400

    hi

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -32,17 +32,70 @@ angular.module('addicaidSiteApp')
     };
 
 
-
-
-
-
     $scope.mapOptions = {
 //      center: new google.maps.LatLng(40.763562,-73.97140100000001),
       center: new google.maps.LatLng(42.25113,-73.791435),
       zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      styles: [
+  {
+    "featureType": "water",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "color": "#4ebbea" },
+      { "saturation": -13 }
+    ]
+  },{
+    "featureType": "road.highway",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "color": "#e7e9e7" }
+    ]
+  },{
+  },{
+    "elementType": "geometry.stroke",
+    "stylers": [
+      { "color": "#b2b2b1" }
+    ]
+  },{
+    "featureType": "road.arterial",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "color": "#ffffff" }
+    ]
+  },{
+    "featureType": "road.arterial",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      { "visibility": "on" },
+      { "saturation": -2 },
+      { "color": "#ffffff" }
+    ]
+  },{
+    "elementType": "labels.text.fill",
+    "stylers": [
+      { "color": "#060606" },
+      { "lightness": 23 }
+    ]
+  },{
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      { "color": "#ffffff" }
+    ]
+  },{
+    "featureType": "poi.park",
+    "elementType": "geometry.fill",
+    "stylers": [
+      { "hue": "#b2ff00" },
+      { "gamma": 1.07 },
+      { "saturation": 52 },
+      { "lightness": -18 }
+    ]
+  },{
+  }
+]
     };
-
+/*
     $http.get('styles/mapStyle.json')
       .success(function(data, status) {
         console.log('success', data, status);
@@ -51,5 +104,5 @@ angular.module('addicaidSiteApp')
       .error(function(data,status) {
         // TODO: error handling
         console.error('FAILURE', data, status);
-      });
-  }]);
+      });*/
+  }]); 
