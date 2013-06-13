3d2351c92a1dfcef01ad5d98f9fc5c0bc6cd5ff9
Matthew Kleiman <bigmickey@gmail.com>
2013-06-13 17:19:36 -0400

    hide mobile header/footer

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,8 +2,7 @@
 
 angular.module('addicaidSiteApp')
   .controller('MainCtrl', ['$scope', '$location', 'browserDetection', '$rootScope', function($scope, $location, browserDetection, $rootScope) {
-//    $scope.useMobile = $location.path() === '/mobile';
-//    console.log('MainCtrl useMobile='+$scope.useMobile, $location.path());
+    $rootScope.useMobileHeaderFooter = $location.path().slice(0,7) === '/mobile';
 
 
   }]);
